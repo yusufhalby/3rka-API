451d84f7abee3df57937bfd2a7b4128167156b4b
yusufhalby <yosufkhaled@eng.aswu.edu.eg>
2023-08-17 00:13:13 +0300

    unconfirmed fights handlers (get, accept, delete)

--- controllers/fights.js
+++ controllers/fights.js
@@ -1,37 +1,63 @@
+const Sequelize = require('sequelize');
 const { Op } = require('sequelize');
 
 const Fights = require('../models/fights');
 const Menfights = require('../models/menfights');
 const Men = require('../models/men');
 
-exports.orderFight = async (req, res, next) =>{
-    const {ClientName, FightAddress, FightTime, phone, details, email, selectedIDs} = req.body
-    try{
-    const fight = await Fights.create({ClientName, FightAddress, FightTime, phone, details, email});
-    const FightID = fight.FightID;
-    await selectedIDs.forEach(async ManID => {
-        await Menfights.create({ManID, FightID});
-    });
-    return res.status(201).json({'message': 'success', fight});
-} catch (err) {
-    if (!err.statusCode) {
-        err.statusCode = 500;
+exports.orderFight = async (req, res, next) => {
+    const {
+        ClientName,
+        FightAddress,
+        FightTime,
+        phone,
+        details,
+        email,
+        selectedIDs
+    } = req.body
+    try {
+        const fight = await Fights.create({
+            ClientName,
+            FightAddress,
+            FightTime,
+            phone,
+            details,
+            email
+        });
+        const FightID = fight.FightID;
+        await selectedIDs.forEach(async ManID => {
+            await Menfights.create({
+                ManID,
+                FightID
+            });
+        });
+        return res.status(201).json({
+            'message': 'success',
+            fight
+        });
+    } catch (err) {
+        if (!err.statusCode) {
+            err.statusCode = 500;
+        }
+        next(err);
     }
-    next(err);
-}
 };
 
-exports.getConfirmedFights = async (req, res, next) =>{
+exports.getConfirmedFights = async (req, res, next) => {
     const ManID = req.params.ManID;
-    try{
+    try {
         const fights = await Menfights.findAll({
             include: {
                 model: Fights,
-                where: {isConfirmed: 1},
+                where: {
+                    isConfirmed: 1
+                },
                 // attributes: [],
                 attributes: ['ClientName', 'FightAddress', 'FightTime', 'phone', 'details', 'email'],
             },
-            where: {ManID},
+            where: {
+                ManID
+            },
             attributes: ['FightID'],
             raw: true,
             nest: true,
@@ -44,27 +70,163 @@ exports.getConfirmedFights = async (req, res, next) =>{
         const fightsIds = fights.map(f => {
             return f.FightID;
         });
-        const men = await Menfights.findAll({
+        const fighters = await Menfights.findAll({
             include: [{
                 model: Men,
-                where: {isConfirmed: 1},
+                where: {
+                    isConfirmed: 1
+                },
                 attributes: [
                     'name',
                     'ManID'
                 ]
-            },
-        ],
+            }, ],
             attributes: ["FightID"],
-            where: {FightID: fightsIds, ManID: {[Op.ne]: ManID}}
+            where: {
+                FightID: fightsIds,
+                ManID: {
+                    [Op.ne]: ManID
+                }
+            }
         });
         fights.forEach(f => {
             f.men = [];
-            men.forEach(m => {
+            fighters.forEach(m => {
                 if (f.FightID == m.FightID && m.man.ManID != ManID) f.men.push(m.man)
             });
+            const {
+                FightID,
+                fight,
+                men
+            } = f;
+            Object.assign(f, fight);
+            delete f.fight;
+            f.FightID = FightID;
+            f.men = men;
         });
-        res.status(200).json({message: 'Fights fetched successfully', fights});
-    }catch (err) {
+        res.status(200).json({
+            message: 'Fights fetched successfully',
+            fights
+        });
+    } catch (err) {
+        if (!err.statusCode) {
+            err.statusCode = 500;
+        }
+        next(err);
+    }
+};
+
+exports.geUnconfirmedtFights = async (req, res, next) => {
+    try {
+        const fights = await Menfights.findAll({
+            include: {
+                model: Fights,
+                where: {
+                    isConfirmed: 0
+                },
+                attributes: ['ClientName', 'FightAddress', 'FightTime', 'phone', 'details', 'email'],
+            },
+            attributes: [
+                [Sequelize.fn('DISTINCT', Sequelize.col('fight.FightID')), 'FightID'],
+            ],
+            raw: true,
+            nest: true,
+        });
+        if (fights.length == 0 || !fights) {
+            const error = new Error('No fights found.');
+            error.statusCode = 404;
+            throw error;
+        }
+        const fightsIds = fights.map(f => {
+            return f.FightID;
+        });
+        const fighters = await Menfights.findAll({
+            include: [{
+                model: Men,
+                where: {
+                    isConfirmed: 1
+                },
+                attributes: [
+                    'name',
+                    'ManID'
+                ]
+            }, ],
+            attributes: ["FightID"],
+            where: {
+                FightID: fightsIds
+            }
+        });
+        fights.forEach(f => {
+            f.men = [];
+            fighters.forEach(m => {
+                if (f.FightID == m.FightID) f.men.push(m.man);
+            });
+            const {
+                FightID,
+                fight,
+                men
+            } = f;
+            Object.assign(f, fight);
+            delete f.fight;
+            f.FightID = FightID;
+            f.men = men;
+        });
+        res.status(200).json({
+            message: 'Fights fetched successfully',
+            fights
+        });
+    } catch (err) {
+        if (!err.statusCode) {
+            err.statusCode = 500;
+        }
+        next(err);
+    }
+};
+
+exports.acceptFight = async (req, res, next) => {
+    const {
+        FightID
+    } = req.body;
+    try {
+        const fight = await Fights.findByPk(FightID);
+        if (!fight) {
+            const error = new Error('No fight found.');
+            error.statusCode = 404;
+            throw error;
+        }
+        fight.isConfirmed = 1;
+        const updatedFight = await fight.save();
+        res.status(200).json({
+            message: 'Fight was accepted successfully',
+            fight: updatedFight
+        });
+    } catch (err) {
+        if (!err.statusCode) {
+            err.statusCode = 500;
+        }
+        next(err);
+    }
+};
+
+exports.deleteFight = async (req, res, next) => {
+    const FightID = req.params.FightID;
+    try {
+        const fight = await Fights.findByPk(FightID);
+        if (!fight) {
+            const error = new Error('No fight found.');
+            error.statusCode = 404;
+            throw error;
+        };
+        await Menfights.destroy({
+            where: {
+                FightID
+            }
+        });
+        await fight.destroy();
+        res.status(200).json({
+            message: 'Fight was deleted successfully'
+        });
+    } catch (err) {
         if (!err.statusCode) {
             err.statusCode = 500;
         }
--- routes/admin.js
+++ routes/admin.js
@@ -1,11 +1,14 @@
 const router = require('express').Router();
 
 const menController = require('../controllers/men');
-
+const fightsController = require('../controllers/fights');
 
 router.get('/men', menController.geUnconfirmedtMen);
-router.post('/men', menController.acceptMan);
+router.patch('/men', menController.acceptMan);
 router.delete('/men/:ManID', menController.deleteMan);
 
+router.get('/fights', fightsController.geUnconfirmedtFights);
+router.patch('/fights', fightsController.acceptFight);
+router.delete('/fights/:FightID', fightsController.deleteFight);
 
 module.exports = router;
